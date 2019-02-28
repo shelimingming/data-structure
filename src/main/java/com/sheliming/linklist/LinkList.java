1f6a9cf4770bffec758ae4bec7b466999f52e069
shelimingming <shelimingming@qq.com>
2019-03-01 00:29:55 +0800

    增加合并两个有序链表

--- src/main/java/com/sheliming/linklist/LinkList.java
+++ src/main/java/com/sheliming/linklist/LinkList.java
@@ -27,16 +27,21 @@ public class LinkList {
     }
 
     //打印链表
-    public void print(Node node) {
+    public String print(Node node) {
         if (node == null) {
-            return;
+            return null;
         }
+
+        StringBuilder sb = new StringBuilder();
 
         Node current = node;
         while (current != null) {
-            System.out.print(current.data + ",");
+            sb.append(current.data + ",");
             current = current.next;
         }
+
+        System.out.println(sb.substring(0, sb.length() - 1));
+        return sb.substring(0, sb.length() - 1);
     }
 
     //返回链表中节点的个数
@@ -61,7 +66,7 @@ public class LinkList {
 //    然后整体向后移动这两个节点，直到second节点走到最后一个结点的时候，
 //    此时first节点所指向的位置就是倒数第k个节点的位置。时间复杂度为O（n）
     public Node findListNode(int index) {
-        if(head==null||index==0) {
+        if (head == null || index == 0) {
             return null;
         }
 
@@ -69,20 +74,76 @@ public class LinkList {
         Node second = head;
 
 
-        for(int i=0;i<index-1;i++) {
+        for (int i = 0; i < index - 1; i++) {
             second = second.next;
-            if(second == null) {
+            if (second == null) {
                 //说明index的值已经大于链表的长度了
                 return null;
             }
         }
 
-        while (second.next!=null) {
+        while (second.next != null) {
             first = first.next;
             second = second.next;
         }
 
         return first;
+    }
+
+    /**
+     * 合并两个有序的单链表，合并之后的链表依然有序：
+     *
+     * @param head1
+     * @param head2
+     * @return
+     */
+    public Node mergeLinkList(Node head1, Node head2) {
+        if(head1 ==null&&head2==null) {
+            return null;
+        }
+        if(head1 == null) {
+            return head2;
+        }
+        if(head2 ==null) {
+            return head1;
+        }
+
+        Node head;
+        Node current;
+
+        //一开始，我们让current结点指向head1和head2中较小的数据，得到head结点
+        if(head1.data < head2.data) {
+            head = head1;
+            current = head1;
+            head1 = head1.next;
+        }else {
+            head = head2;
+            current = head2;
+            head2 = head2.next;
+        }
+
+        while(head1!=null&&head2!=null) {
+            if(head1.data<head2.data) {
+                current.next = head1;
+                current = head1;
+                head1 = head1.next;
+            } else {
+                current.next = head2;
+                current = head2;
+                head2 = head2.next;
+            }
+        }
+
+        //合并剩余的元素
+        if(head1 == null) {
+            current.next = head2;
+        }
+
+        if(head2 == null) {
+            current.next = head1;
+        }
+
+        return head;
     }
 
     public static void main(String[] args) {
--- src/main/test/com/sheliming/linklist/LinkListTest.java
+++ src/main/test/com/sheliming/linklist/LinkListTest.java
@@ -1,7 +1,6 @@
 package com.sheliming.linklist;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class LinkListTest {
@@ -35,5 +34,27 @@ public class LinkListTest {
         Assert.assertEquals("返回不正确", 7, linkList.findListNode(3).data);
         Assert.assertEquals("返回不正确", 0, linkList.findListNode(10).data);
 
+    }
+
+    /**
+     * 合并两个链表
+     */
+    @Test
+    public void mergeLinkListTest() {
+        LinkList list1 = new LinkList();
+        LinkList list2 = new LinkList();
+        //向LinkList中添加数据
+        for (int i = 0; i < 4; i++) {
+            list1.add(i);
+        }
+
+        for (int i = 3; i < 8; i++) {
+            list2.add(i);
+        }
+
+        LinkList list3 = new LinkList();
+        list3.head = list3.mergeLinkList(list1.head, list2.head); //将list1和list2合并，存放到list3中
+
+        Assert.assertEquals("0,1,2,3,3,4,5,6,7", list3.print(list3.head));
     }
 }
